dbecec56e45e4fbfa754f7dd89b58891775a1953
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-05-10 16:19:58 +0600

    lets start

---
+++ app.js
@@ -0,0 +1,17 @@
+function sleep(names, time) {
+  console.log(names + " is sleeping from " + time);
+}
+console.log("hello, this is abdullah jubayer");
+
+sleep("Akram", "10 PM");
+
+sleep("Jasim", "12 PM");
+sleep("Kawsar", "9 AM");
+
+function myFunction(a, b) {
+  return a * b;
+}
+
+let x = myFunction(5, 10);
+
+console.log(x);
